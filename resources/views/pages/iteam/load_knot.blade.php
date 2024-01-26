659d764d98c8bb7180a40125834288f2add5ec99
chidokato <tuan.pn92@gmail.com>
2024-01-26 13:40:06 +0700

    sdf

--- resources/views/pages/iteam/load_knot.blade.php
+++ resources/views/pages/iteam/load_knot.blade.php
@@ -42,3 +42,50 @@
         @endif
     @endforeach
 @endif
+
+
+<script type="text/javascript">
+    function body(event){
+        event.preventDefault();
+        let urlbody = $(this).data('url');
+        $.ajax({
+            type: 'GET',
+            url: urlbody,
+            dataType: 'json',
+            success: function (data){
+                if(data.code === 200){
+                    $('.mat').html(data.mat_html)
+                    $('.face').html(data.mat_html)
+                }
+            },
+            error: function(){
+
+            }
+        });
+    }
+    function strap(event){
+        event.preventDefault();
+        let urlstrap = $(this).data('url');
+        $.ajax({
+            type: 'GET',
+            url: urlstrap,
+            dataType: 'json',
+            success: function (data){
+                if(data.code === 200){
+                    $('.str_m').html(data.str_m)
+                    $('.flat_top').html(data.flat_top)
+                    $('.flat_bottom').html(data.flat_bottom)
+                }
+            },
+            error: function(){
+
+            }
+        });
+    }
+
+
+    $(document).ready(function(){
+        $('.clik_body').on('click', body);
+        $('.clik_strap').on('click', strap);
+    });
+</script>
