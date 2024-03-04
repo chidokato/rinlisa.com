f88ceb57ed65a7a6dc4a9fbf51901d9454c2375e
= <=>
2024-03-04 21:39:38 +0700

    update

--- resources/views/pages/iteam/load_cat.blade.php
+++ resources/views/pages/iteam/load_cat.blade.php
@@ -3,7 +3,7 @@
     $setting = Setting::find('1');
 ?>
 @foreach($post as $val)
-<div class="col-lg-4 col-md-4 col-12 ">
+<div class="col-lg-4 col-md-4 col-6 ">
     @include('pages.iteam.product')
 </div>
 @endforeach
