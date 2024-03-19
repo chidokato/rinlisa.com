7f52803464498e033e6293a5783b01ae62cbeacb
chidokato <tuan.pn92@gmail.com>
2024-03-19 15:54:18 +0700

    sdf

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -262,15 +262,20 @@
             <div class="row">
                 <div class="col-12">
                     <div class="single_banner banner_fullwidth">
+                        @foreach($slider as $key => $val)
+                        @if($val->note == 'banner 3')
                         <div class="banner_thumb">
-                            <a href="#"><img src="assets/img/bg/banner5.jpg" alt=""></a>
-                            <div class="banner_text">
+                            <a href="{{$val->link}}"><img src="data/home/800/{{$val->img}}" alt=""></a>
+                            <!-- <div class="banner_text">
                                 <h2>Win the cost of your</h2>
                                 <h3>Tyres back</h3>
                                 <p>Chance to win when you buy 2 + Pirell Tyres in March</p>
                                 <a href="shop.html">Discover Now</a>
-                            </div>
+                            </div> -->
                         </div>
+                        @endif
+                        @endforeach
+                        
                     </div>
                 </div>
             </div>
