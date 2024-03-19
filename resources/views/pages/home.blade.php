b2e1c8040c1c8f00472a79e1a0f1231bdcd324b7
chidokato <tuan.pn92@gmail.com>
2024-03-19 15:42:14 +0700

    sdf

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -213,26 +213,20 @@
             <div class="row">
                 <div class="col-12">
                     <div class="banner_container">
+                        @foreach($slider as $key => $val)
+                        @if($val->note == 'banner 2')
                         <div class="single_banner">
                             <div class="banner_thumb">
-                                <a href="#"><img src="assets/img/bg/banner3.jpg" alt=""></a>
-                                <div class="banner_text">
+                                <a href="{{$val->link}}"><img src="data/home/800/{{$val->img}}" alt=""></a>
+                                <!-- <div class="banner_text">
                                     <h3>Car Audio</h3>
                                     <h2>Super Natural Sound</h2>
                                     <a href="shop.html">Shop Now</a>
-                                </div>
+                                </div> -->
                             </div>
                         </div>
-                        <div class="single_banner">
-                            <div class="banner_thumb">
-                                <a href="#"><img src="assets/img/bg/banner4.jpg" alt=""></a>
-                                <div class="banner_text">
-                                    <h3>All - New</h3>
-                                    <h2>Perfomance Parts</h2>
-                                    <a href="shop.html">Shop Now</a>
-                                </div>
-                            </div>
-                        </div>
+                        @endif
+                        @endforeach
                     </div>
                 </div>
             </div>
